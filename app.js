0d7fa576c72b71c0d5e02ea44f3b260742fd6ece
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-01-19 13:15:29 +0530

    Lecture 31 Encoding User Input

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const key = 'AIzaSyBxU9MTrAHV1ivTuOQA_yaoH779S2lN2QI';
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
+
+let encodedAddress = encodeURIComponent(argv.a);
+
 request({
-        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${key}`,
     json: true
-    }, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-    console.log('body:', body); // Print the HTML for the Google homepage.
-    console.log(JSON.stringify(body, undefined, 2));
+}, (error, response, body) => {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    if(body.results[0]) {
+        console.log('Address: ', body.results[0].formatted_address);
+        console.log('Latitude: ', body.results[0].geometry.location.lat);
+        console.log('Longitude: ', body.results[0].geometry.location.lng);
+    }else {
+        console.log("Status : " + body.status);
+    }
 });
+
+
